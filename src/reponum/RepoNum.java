72d7c29e5c9ac0252887c39853e662bce475b9c1
mauricio tolosa <tolosamauricio@hotmail.com>
2023-10-23 15:31:56 -0300

    activar
    escritura sout

---
+++ src/reponum/RepoNum.java
@@ -0,0 +1,12 @@
+package reponum;
+
+public class RepoNum {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Nuevo repo activado");
+        System.out.println("----- line -----");
+    }
+    
+}
